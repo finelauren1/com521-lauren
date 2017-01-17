db66a2860991571e4a627c5eb6b9978875bc5c3e
finelauren1 <finel@uw.edu>
2017-01-17 13:12:04 -0800

    added week3 programming challenge

---
+++ week3 programing challenge lauren.R
@@ -0,0 +1,34 @@
+setwd("/users/laurenfine/desktop/onedrive/winter 2017/statistics/com521-lauren")
+library(readr)
+> week3_dataset_lauren <- read_csv("/users/laurenfine/desktop/onedrive/winter 2017/statistics/com521-lauren/week3_dataset-lauren.csv")
+
+load("~/Desktop/OneDrive/Winter 2017/Statistics/com521-lauren/week2_dataset-lauren copy.RData")
+
+summary(week3_dataset_lauren)
+apply(week3_dataset_lauren, 2, sd)
+nrow(week3_dataset_lauren)
+ncol(week3_dataset_lauren)
+names(week3_dataset_lauren)
+head(week3_dataset_lauren)
+week3_dataset_lauren$x
+
+hist(week3_dataset_lauren$x)
+hist(week3_dataset_lauren$y)
+hist(week3_dataset_lauren$j)
+hist(week3_dataset_lauren$k)
+hist(week3_dataset_lauren$i)
+
+summary(week2.dataset)
+summary(week3_dataset_lauren$x)
+boxplot(week3_dataset_lauren$x, week2.dataset)
+hist(week3_dataset_lauren$x)
+hist(week2.dataset)
+
+ggplot(data=week3_dataset_lauren)+geom_point()+aes(x=x, y=y)
+
+week3_dataset_lauren$j<-as.logical(week3_dataset_lauren$j)
+week3_dataset_lauren$i<-as.logical(week3_dataset_lauren$i)
+week3_dataset_lauren$k<-as.factor(week3_dataset_lauren$k)
+
+ggplot(data=week3_dataset_lauren)+geom_point()+aes(x=x, y=y, color=i, shape=j, size=k)
+
